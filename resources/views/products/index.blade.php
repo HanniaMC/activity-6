110ea58433ad47d947ae20e740ae7cbad1a9f590
HanniaMC <hanniamichellem@gmail.com>
2026-03-01 14:40:05 -0700

    homework 5

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,15 @@
-<title>Product View</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Product View</title>
+</head>
+<body>
+  <h1>Products</h1>
+
+  <a href="{{ route('products.create') }}">
+    <button type="button">Add Product</button>
+  </a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
+// Página principal (opcional)
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para Products (solo las que pide la tarea)
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
